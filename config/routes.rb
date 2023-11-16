25d9edcd7924659872a1136cfea4089585e56483
Fabio Freitas <fhcfreitas@gmail.com>
2023-11-16 11:43:57 -0300

    watch list setup

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,9 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
+  # resources :lists, only: %i[index show new create] do
+  #   resources :bookmarks, only: %i[new create destroy] do
+  #   end
+  # end
   # root "posts#index"
 end
